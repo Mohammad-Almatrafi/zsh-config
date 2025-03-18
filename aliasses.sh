2afdb573e7553d96984c9c4d91a99903b99a12b4
Rur1k <moalmatrafi90@gmail.com>
2025-03-18 20:50:14 +0300

    initial commit

---
+++ aliasses.sh
@@ -0,0 +1,9 @@
+
+alias vivado="mkdir ~/vivado_log
+cd ~/vivado_log
+~/Xilinx/Vivado/Vivado/2019.1/bin/vivado
+cd ..
+rm -rf ~/vivado_log
+"
+
+alias ls="colorls"
---
+++ zshConfig.sh
@@ -0,0 +1,9 @@
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+plugins=(
+    git 
+    zsh-syntax-highlighting 
+    zsh-autosuggestions
+)
