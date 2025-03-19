4f385543fdfb5294ad154c14adf2f1135d599378
Rur1k <moalmatrafi90@gmail.com>
2025-03-19 03:10:03 +0300

    added relative adressing to the zshConfig file

---
+++ readme.md
@@ -0,0 +1,7 @@
+# ZSH terminal config
+you need the oh-my-zsh plugin manager and the following zsh plugins:
+- zsh-syntax-highlighting 
+- zsh-autosuggestions
+- powerlevel10k theme
+
+remove everything in your .zshrc and source the zshConfig.sh file 
--- zshConfig.sh
+++ zshConfig.sh
@@ -1,5 +1,7 @@
 export ZSH="$HOME/.oh-my-zsh"
 
+COSTUM_CONFIG_PATH="${funcsourcetrace[1]%/*}/"
+
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 plugins=(
@@ -7,3 +9,12 @@ plugins=(
     zsh-syntax-highlighting 
     zsh-autosuggestions
 )
+
+source $ZSH/oh-my-zsh.sh
+
+source "${COSTUM_CONFIG_PATH}aliasses.sh"
+
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+export PATH="$PATH:/opt/riscv/bin"
